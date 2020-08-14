573cd5d94d7f6c007ab816393e51383db27d8d25
Ishmam156 <ishmamchowdhurygp@gmail.com>
2020-08-14 11:04:27 +0600

    refresh

--- onlinedu/context_processors.py
+++
@@ -1,6 +0,0 @@
-from .models import CLASS_CHOICES
-
-# Global categories for navbar dropdown
-def get_category_choices(request):
-    categories = [i[0] for i in CLASS_CHOICES]
-    return {'categories' : categories }
